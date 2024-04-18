eff595026d976a28a9036b690167960b1d0ba794
NaraZambrano <lourdes.zambrano@ikasle.aeg.eus>
2024-04-18 12:11:45 +0200

    nara y sus problemas con git

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,41 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, } from 'react'
+
+
 import './App.css'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [comments, setComments] = useState([])
+  const title = 'Welcome to the Commentes'
+  const handleClick = (e) => {
+    console.log(e)
+    axios.get("https://jsonplaceholder.typicode.com/comments")
+      .then((response) => {
+        setComments (response.data)
+      })
+      .catch ((error) => {
+        console.log(error)
+      })
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='container'>
+      <div className='header'>
+        <h1> {title}</h1>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+      <div className='button'>
+        <button className='button-click' onClick={handleClick}>Lunch</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className='content-comments'>
+        <div className='mesages'>
+
+        </div>
+        
+      </div>
+    </div>
+    
+     
     </>
   )
 }
